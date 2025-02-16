c6becce07e924ae71c92338506da063453b4e547
Sanya Singh <112766278+sxnyx@users.noreply.github.com>
2025-02-16 17:37:43 +0530

    Update SQL Query codes.sql

--- SQL Query codes.sql
+++ SQL Query codes.sql
@@ -67,8 +67,8 @@ SELECT DISTINCT most_played_on FROM spotify
 
 --Solving Problems
 
--------------------------
------Easy Level-----------
+--------------------------
+-------Easy Level---------
 --------------------------
 
 --1. Retrieve the names of all tracks that have more than 1 billion streams.
@@ -98,3 +98,59 @@ SELECT
 FROM spotify
 GROUP BY artist
 ORDER BY 2 DESC
+
+
+---------------------------
+-------Medium Level--------
+---------------------------
+
+-- 1. Calculate the average danceability of tracks in each album
+SELECT 
+	album, -- 1
+	avg(danceability) as avg_danceability
+FROM spotify
+GROUP BY 1
+ORDER BY 2 Desc
+
+-- 2. Find the top 5 tracks with the highest energy values
+SELECT
+	track,
+	MAX(energy)
+FROM spotify
+GROUP BY 1
+ORDER BY 2 Desc
+LIMIT 5
+
+-- 3. List all tracks along with their views and likes where official_video = TRUE
+SELECT
+	track,
+	SUM(views) as Total_views,
+	SUM(likes) as Total_likes
+FROM spotify
+WHERE official_video = 'true'
+GROUP BY 1
+ORDER BY 2 Desc
+
+--4. For each album, calculate the total views of all associated tracks
+SELECT 
+	album,
+	track,
+	SUM(views)
+FROM spotify
+GROUP BY 1, 2
+ORDER BY 3 DESC
+
+--5. Retrieve the track names that have been streamed on Spotify more than YouTube
+SELECT * FROM
+(SELECT 
+	track,
+	COALESCE(SUM(CASE WHEN most_played_on ='YouTube' THEN stream END),0) as streamed_on_youtube,
+	COALESCE(SUM(CASE WHEN most_played_on ='Spotify' THEN stream END),0) as streamed_on_spotify	
+FROM spotify
+GROUP BY 1
+) AS table1
+WHERE 
+	streamed_on_spotify > streamed_on_youtube
+	AND
+	streamed_on_youtube > 0
+
